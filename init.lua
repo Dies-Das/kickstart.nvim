eeb67914fbfe714865a2da354c87e5875751bde8
Tilman Aleman <aleman@igpm.rtwh-aachen.de>
2025-12-03 09:08:55 +0100

    laptop config

--- init.lua
+++ init.lua
@@ -90,8 +90,7 @@ P.S. You can delete this when you're done too. It's your config now! :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -102,7 +101,7 @@ vim.g.have_nerd_font = false
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
@@ -117,7 +116,12 @@ vim.o.showmode = false
 vim.schedule(function()
   vim.o.clipboard = 'unnamedplus'
 end)
-
+-- vim.api.nvim_create_autocmd('FileType', {
+--   pattern = { 'tex', 'plaintex', 'latex' },
+--   callback = function()
+--     vim.opt.spell = false
+--   end,
+-- })
 -- Enable break indent
 vim.o.breakindent = true
 
@@ -184,6 +188,11 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+-- Next buffer
+vim.keymap.set('n', '<Tab>', ':bnext<CR>', { silent = true, desc = 'Next buffer' })
+
+-- Previous buffer
+vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', { silent = true, desc = 'Previous buffer' })
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 -- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
@@ -438,7 +447,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
-      vim.keymap.set('n', '<leader>/', function()
+      vim.keymap.set('n', '<leader>.', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
         builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
           winblend = 10,
@@ -671,9 +680,9 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -683,7 +692,22 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
-
+        ltex_plus = {
+          filetypes = { 'tex', 'plaintex', 'markdown', 'text' },
+          settings = {
+            ltex_plus = {
+              language = 'en-US', -- or "en-GB"
+              -- Optional: add custom dictionaries
+              dictionary = {
+                ['en-US'] = {
+                  'Neovim',
+                  'TeX',
+                  'LaTeX',
+                },
+              },
+            },
+          },
+        },
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -769,7 +793,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'isort', 'black' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
@@ -944,7 +968,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'python' }, --, 'latex', 'bibtex' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
@@ -973,18 +997,18 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,60 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'numToStr/Comment.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+      require('Comment').setup {
+        toggler = {
+          line = '<localleader>/',
+        },
+        opleader = {
+          line = '<localleader>/',
+        },
+      }
+    end,
+  },
+  {
+    'lervag/vimtex',
+    lazy = false, -- load on startup (vimtex likes this)
+    init = function()
+      -- Use latexmk as the default compiler
+      vim.g.vimtex_compiler_method = 'latexmk'
+
+      -- Choose your PDF viewer, adjust this to your system:
+      -- common values: "zathura", "skim", "okular", "sioyek", "general"
+      vim.g.vimtex_view_method = 'zathura'
+
+      -- Don’t use `tex` as a generic filetype for plaintex
+      vim.g.vimtex_filetype_map = { tex = 'latex' }
+
+      vim.g.vimtex_quickfix_mode = 2 -- show errors only
+      vim.g.vimtex_quickfix_open_on_warning = 0 -- don't auto-open on warnings
+      vim.g.vimtex_syntax_enabled = 0
+      vim.g.vimtex_imaps_enabled = 1
+      vim.g.vimtex_imaps_leader = ';' -- for example
+      -- Optional: disable vimtex mappings if they clash
+      -- vim.g.vimtex_mappings_enabled = 0
+    end,
+  },
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('bufferline').setup {}
+    end,
+  },
+  {
+    'SirVer/ultisnips',
+    dependencies = { 'honza/vim-snippets' }, -- optional, but gives a lot of LaTeX snippets
+    init = function()
+      vim.g.UltiSnipsExpandTrigger = '<tab>'
+      vim.g.UltiSnipsJumpForwardTrigger = '<tab>'
+      vim.g.UltiSnipsJumpBackwardTrigger = '<s-tab>'
+    end,
+  },
+  { 'akinsho/toggleterm.nvim', version = '*', config = true },
+}
